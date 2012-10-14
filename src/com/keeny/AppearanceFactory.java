58f642b98fabd0a05c4296b46e4c4f2ef8c93227
keeny <barry@craftstudios.co.nz>
2012-10-14 23:15:31 +1300

    Swtich to Factory instead of static class

---
+++ src/com/keeny/AppearanceFactory.java
@@ -0,0 +1,11 @@
+package com.keeny;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: keeny
+ * Date: 14/10/12
+ * Time: 11:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppearanceFactory {
+}
